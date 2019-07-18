68abbcb40eeb065dd5fd04add0360f091a6da346
Daniel Wadell <danielwadell@Daniels-MacBook-Pro.local>
2019-07-17 22:18:26 -0700

    Added the Comment model in busyapp/models.py

--- busyapp/models.py
+++ busyapp/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
-    title = models.CharField(max_length=264)
+    title = models.CharField(max_length=264,unique=True)
     text = models.TextField()
     image = models.ImageField(upload_to="post_images")
     create_date = models.DateTimeField(default=timezone.now())
@@ -16,7 +16,21 @@ class Post(models.Model):
         self.save()
     
     def approve_comments(self):
-        return self.comments.filter(approve_comments=True)
+        return self.comments.filter(approve_comment=True)
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=264)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
