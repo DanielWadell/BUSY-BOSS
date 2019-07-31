f3eb01bdc9be20b17454dd68bc09b23fd7c817b1
Daniel Wadell <danielwadell@Daniels-MacBook-Pro.local>
2019-07-31 13:45:27 -0700

    Displayed all posts on the home page

--- busyapp/urls.py
+++ busyapp/urls.py
@@ -5,10 +5,12 @@ from django.contrib import admin
 app_name = 'busyapp'
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('', views.index, name='index'),
     path('register_user/',views.register_user,name='register_user'),
     path('log_in/',views.user_login,name='login'),
     path('log_out/',views.user_logout,name='logout'),
+    path('post/publish/', views.post_publish, name='post_publish'),
+    path('post/details/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/create/',views.create_post,name='create_post'),
     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
--- busyapp/views.py
+++ busyapp/views.py
@@ -8,9 +8,13 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse,reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (TemplateView,ListView,
+                                  DetailView,CreateView,
+                                  UpdateView,DeleteView)
 
 def index(request):
-    return render(request,'busyapp/index.html')
+    context = {'posts':Post.objects.all()}
+    return render(request,'busyapp/index.html',context)
 
 def is_member(user):
     return user.groups.filter(name='Member').exists()
@@ -110,6 +114,18 @@ def start_post(request):
     form = PostForm()
     return render(request, 'busyapp/post_form.html',{'post':form})
 
+class PostDetailView(DetailView):
+    model = Post
+    # template_name = 'busyapp/post_list.html'
+    # context_object_name = 'post'
+
+class PostListView(ListView):
+    model = Post
+    # template_name = 'busyapp/post_list.html'
+    # context_object_name = 'post'
+    # def get_queryset(self):
+    #     return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
 @login_required
 def create_post(request):
     # author = get_author(request.user.username)
@@ -119,7 +135,7 @@ def create_post(request):
             post = post_form.save(commit=False)
             post.author = request.user
             post.save()
-            return HttpResponseRedirect(reverse('busyapp:index'))
+            return reverse_lazy('busyapp:index')
         else:
             print(post_form.errors)
             post = PostForm()
@@ -132,7 +148,7 @@ def create_post(request):
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = CommentForm(request.POST)
+        form = CommentForm(data=request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
@@ -149,6 +165,11 @@ def comment_approve(request, pk):
     comment.approve()
     return redirect('post_detail', pk=comment.post.pk)
 
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
 
 @login_required
 def comment_remove(request, pk):
